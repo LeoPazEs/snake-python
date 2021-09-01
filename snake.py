d42c04085e65d58b5e03b22c51d08d076b5311a2
LeoPazEs <leopestevam@hotmail.com>
2021-08-31 21:45:53 -0300

    FINAL

--- snake.py
+++ snake.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 from tkinter import Tk, Frame, Canvas, ALL, NW 
 
 WIDTH = 300 
-HIGHT = 300 
+HEIGHT = 300 
 DOT_SIZE = 10 
 RAND_POS = 27 
 DELAY = 100 
@@ -12,7 +12,7 @@ DELAY = 100
 class Board(Canvas):
     
     def __init__(self,parent):
-        super().__init__(width=WIDTH, hight=HIGHT,background='black',highlightthickness=0) 
+        super().__init__(width=WIDTH, height=HEIGHT,background='black',highlightthickness=0) 
 
         self.parent = parent
         self.ini_game()
@@ -32,46 +32,150 @@ class Board(Canvas):
             self.ibody = Image.open('dot.png') 
             self.body = ImageTk.PhotoImage(self.ibody)
             self.ihead = Image.open('head.png') 
-            self.body = ImageTk.PhotoImage(self.ihead)
+            self.head = ImageTk.PhotoImage(self.ihead)
             self.idot = Image.open('dot.png') 
             self.dot = ImageTk.PhotoImage(self.idot)
+
         except IOError as e:
             print(str(e))
             sys.exit(1) 
 
         self.focus_get() 
-        self.bind_all('<Key>',self.on_key_press)
-        self.after(DELAY,self.on_time) 
+        self.create_objects()
+        self.bind_all('<Key>',self.on_key_pressed)
+        self.after(DELAY,self.on_time)
 
-        def create_objects(self):
+    def create_objects(self):
 
-            self.create_image(self.dot_x, self.dot_y, image=self.dot, anchor=NW, tag='dot')
-            self.create_image(50, 50, image=self.head, anchor=NW, tag='head')
-            self.create_image(40, 50, image=self.body, anchor=NW, tag='body')
-            self.create_image(30, 50, image=self.body, anchor=NW, tag='body') 
-        
-        def check_dot(self):
-            dot = self.find_withtag('dot')
-            head = self.find_withtag('head') 
+        self.create_image(self.dot_x, self.dot_y, image=self.dot, anchor=NW, tag='dot')
+        self.create_image(50, 50, image=self.head, anchor=NW, tag='head')
+        self.create_image(40, 50, image=self.body, anchor=NW, tag='body')
+        self.create_image(30, 50, image=self.body, anchor=NW, tag='body') 
+    
+    def check_dot(self):
+        dot = self.find_withtag('dot')
+        head = self.find_withtag('head') 
 
-            x1, y1, x2 , y2 = self.bbox(head) 
-            overlap = self.find_overlapping(x1, y1 , x2, y2) 
+        x1, y1, x2 , y2 = self.bbox(head) 
+        overlap = self.find_overlapping(x1, y1 , x2, y2) 
 
+        for ovr in overlap:
+            if dot[0] == ovr:
+                x , y = self.coords(dot)
+                self.create_image(x, y, image = self.body, anchor=NW, tag ='body') 
+                self.locate_dot() 
+
+    def locate_dot(self):
+        dot = self.find_withtag('dot') 
+        self.delete(dot[0]) 
+        r = random.randint(0, RAND_POS) 
+        self.dot_x = r * DOT_SIZE 
+        r = random.randint(0, RAND_POS)
+        self.dot_y = r * DOT_SIZE
+
+        self.create_image(self.dot_x, self.dot_y, image = self.body, anchor=NW, tag ='dot')  
+
+    def do_move(self):
+        bodys = self.find_withtag('body') 
+        head = self.find_withtag('head') 
+
+        items = bodys + head 
+
+        k=0
+        while(k < len(items) - 1):
+            c1 = self.coords(items[k]) 
+            c2 = self.coords(items[k + 1]) 
+            self.move(items[k], c2[0] -c1[0], c2[1] - c1[1]) 
+            k += 1 
+
+        if self.left: 
+            self.move(head, -DOT_SIZE,0)
+
+        if self.right: 
+            self.move(head, DOT_SIZE,0)
+
+        if self.up: 
+            self.move(head, 0, -DOT_SIZE) 
+
+        if self.down: 
+            self.move(head, 0, DOT_SIZE) 
+
+    def check_collisions(self):
+        bodys = self.find_withtag('body')
+        head = self.find_withtag('head')
+
+        x1, y1, x2, y2 = self.bbox(head)
+        overlap = self.find_overlapping(x1, y1, x2, y2)
+
+        for body in bodys:
             for ovr in overlap:
-                if dot[0] == ovr:
-                    x , y = self.coords(dot)
-                    self.create_image(x, y, image = self.body, anchor=NW, tag ='body') 
-                    self.locate_dot() 
+                if body == ovr:
+                    self.in_game = False
 
-        def locate_dot(self):
-            dot = self.find_withtag('dot') 
-            self.delete(dot[0]) 
-            r = random.randint(0, RAND_POS) 
-            self.dot_x = r * DOT_SIZE 
-            r = random.randint(0, RAND_POS)
-            self.dot_y = r * DOT_SIZE
+        if x1 < 0:
+            self.in_game = False
 
-            self.create_image(self.dot_x, self.dot_y, image = self.body, anchor=NW, tag ='body') 
+        if x1 > WIDTH - DOT_SIZE:
+            self.in_game = False
 
+        if y1 < 0:
+            self.in_game = False
+
+        if y1 > HEIGHT - DOT_SIZE:
+            self.in_game = False 
+
+    def on_key_pressed(self, e):
+        key = e.keysym 
+        if key == 'Left' and not self.right:
+            self.left = True 
+            self.up = False 
+            self.down = False
+        
+        if key == 'Right' and not self.left:
+            self.right = True 
+            self.up = False 
+            self.down = False
+
+        if key == 'Up' and not self.down:
+            self.left = False 
+            self.up = True 
+            self.right = False
+
+        if key == 'Down' and not self.up:
+            self.down = True 
+            self.left = False 
+            self.right = False 
+
+    def on_time(self):
+        if self.in_game:
+            self.check_collisions() 
+            self.check_dot() 
+            self.do_move() 
+            self.after(DELAY,self.on_time)
+        else:
+            self.game_over()
+
+    def game_over(self):
+        self.delete(ALL) 
+        self.create_text(self.winfo_width()/2, self.winfo_height()/2, text='Game Over',fill='White') 
+
+class Snake(Frame):
+
+    def __init__(self,parent):
+        super().__init__(parent) 
+        parent.title('Snake') 
+        self.board = Board(parent) 
+        self.pack() 
+
+def main():
+    root = Tk() 
+    snake = Snake(root) 
+    root.mainloop() 
+
+if __name__ == "__main__":
+    main()
+
+
+        
 
         
